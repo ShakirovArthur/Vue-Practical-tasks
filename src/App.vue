ce2bcc0edd23bf135f98f6d216e13ec66fb12ed9
Shakirov Arthur <shakirov.main@gmail.com>
2023-01-03 21:35:34 +0500

    second lesson

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <my-component />
-    <user-card />
+    <!-- <my-component /> -->
+    <!-- <user-card /> -->
     <!-- <MyComponent/> два способа писать название комппонента-->
+    <ImageCat/>
   </div>
 </template>
 
 <script>
-import MyComponent from "./components/MyComponent.vue";
-import UserCard from "./components/UserCard.vue";
+// import MyComponent from "./components/MyComponent.vue";
+// import UserCard from "./components/UserCard.vue";
+import ImageCat from './components/ImageCat.vue';
 export default {
   name: "App",
   components: {
-    MyComponent,
-    UserCard,
+    // MyComponent,
+    // UserCard,
+    ImageCat
   },
 };
 </script>
